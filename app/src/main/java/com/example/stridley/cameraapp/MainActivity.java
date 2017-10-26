3299044e95d3aa8d3a83c319a6895d1c39d71086
ST Ridley <sridley4@uwo.ca>
2017-10-26 13:22:50 -0400

    Added feature of being able to access pictures from gallery

--- app/src/main/java/com/example/stridley/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/stridley/cameraapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.stridley.cameraapp;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -20,12 +22,14 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     String image;
     RequestQueue queue;
     String URL = "";
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
         queue = Volley.newRequestQueue(this);
 
         Button cameraButton = (Button) findViewById(R.id.cameraButton);
+        Button galleryButton = (Button) findViewById(R.id.galleryButton);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
 
         cameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,37 +50,70 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent,0);
             }
         });
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(galleryIntent, 1);
+            }
+        });
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-        image = encodeBase(bitmap);
+        if (resultCode == this.RESULT_CANCELED) {
+            return;
+        }
+        else if(requestCode == 0)
+        {
+            Bitmap bitmap = (Bitmap)data.getExtras().get("data");
+            imageView.setImageBitmap(bitmap);
+            Toast.makeText(MainActivity.this, "Image captured!", Toast.LENGTH_SHORT).show();
+            image = encodeBase(bitmap);
+        }
+        else if(requestCode == 1)
+        {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                    Toast.makeText(MainActivity.this, "Image retrieved!", Toast.LENGTH_SHORT).show();
+                    imageView.setImageBitmap(bitmap);
+                    image = encodeBase(bitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 
-    private void getAnalysis(String image) {
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
+//   private void getAnalysis(String image) {
+//       JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL,
+//               new Response.Listener<JSONObject>() {
+//                   @Override
+//                   public void onResponse(JSONObject response) {
 
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
+//                   }
+//               },
+//               new Response.ErrorListener() {
+//                   @Override
+//                   public void onErrorResponse(VolleyError error) {
 
-                    }
-                }){
-                    @Override
-                    protected Map<String, String> getParams()
-                    {
-                        return params;
-                    }
-                };
-    }
+//                   }
+//               }){
+//                   @Override
+//                   protected Map<String, String> getParams()
+//                   {
+//                       return params;
+//                   }
+//               };
+//   }
 
     private String encodeBase(Bitmap bm)
     {
